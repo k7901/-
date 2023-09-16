dae22ec3bbde1196761993f0dd878f35af9eb1f1
KokiOhara <144040393+k7901@users.noreply.github.com>
2023-09-16 11:18:49 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
-  
+
 
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -14,10 +14,10 @@ class BooksController < ApplicationController
     else
       @books = Book.all
       flash.now[:alert] = "error"
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
-  
+
 
   def show
     @book = Book.find(params[:id])
@@ -26,21 +26,27 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+    else
+
+       render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
+     flash[:notice] = "Book was successfully destroyed."
     book.destroy
-    redirect_to '/books'
+   redirect_to "/books"
   end
-  
+
    private
-     
+
   #　ストロングパラメータ
   def book_params
     params.require(:book).permit(:title,:body)
